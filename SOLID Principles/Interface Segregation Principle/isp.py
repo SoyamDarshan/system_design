79547b2f894dcd899ebb525dad62aa64be9417bb
Soyam Darshan Pattanaik <32511312+SoyamDarshan@users.noreply.github.com>
2021-02-01 23:18:38 +0530

    added interface segregation principle

---
+++ SOLID Principles/Interface Segregation Principle/isp.py
@@ -0,0 +1,71 @@
+# The idea is we dont want too many methods in a interface, instead keep it granular and keep them separated
+from abc import abstractmethod
+
+
+class Machine:
+    def print(self, document):
+        raise NotImplementedError
+
+    def fax(self, document):
+        raise NotImplementedError
+
+    def scan(self, document):
+        raise NotImplementedError
+
+
+class MultifunctionPrinter(Machine):
+    def print(self, document):
+        pass
+
+    def fax(self, document):
+        pass
+
+    def scan(self, document):
+        pass
+
+
+class OldFashionedPrinter(Machine):
+    def print(self, document):
+        # OK
+        pass
+
+    def fax(self, document):
+        pass  # no operation
+
+    def scan(self, document):
+        raise NotImplementedError('Printer cannot scan!')
+
+
+class Printer:
+    @abstractmethod
+    def print(self, document):
+        pass
+
+
+class Scanner:
+    @abstractmethod
+    def scan(self, document):
+        pass
+
+
+class MyPrinter(Printer):
+    def print(self, document):
+        print(document)
+
+
+class PhotoPrinter(Printer, Scanner):
+    def print(self, document):
+        pass
+
+    def scan(self, document):
+        pass
+
+
+class MultiFunctionDevice(Printer, Scanner):
+    @abstractmethod
+    def print(self, document):
+        pass
+
+    @abstractmethod
+    def scan(self, document):
+        pass
