8c69d5be5d37a0fccd3ab45da26c2c8344d17421
Soyam Darshan Pattanaik <32511312+SoyamDarshan@users.noreply.github.com>
2021-02-03 01:51:25 +0530

    use of builder pattern by following open close principle

---
+++ Creational Patterns/Builder Pattern/builder_inheritance.py
@@ -0,0 +1,48 @@
+class Person:
+    def __init__(self):
+        self.name = None
+        self.position = None
+        self.date_of_birth = None
+
+    def __str__(self):
+        return f'{self.name} born on {self.date_of_birth} works as {self.position}'
+
+    @staticmethod
+    def new():
+        return PersonBuilder()
+
+
+class PersonBuilder:
+    def __init__(self):
+        self.person = Person()
+
+    def build(self):
+        return self.person
+
+
+class PersonInfoBuilder(PersonBuilder):
+    def called(self, name):
+        self.person.name = name
+        return self
+
+
+class PersonJobBuilder(PersonInfoBuilder):
+    def works_as_a(self, position):
+        self.person.position = position
+        return self
+
+
+class PersonBirthDateBuilder(PersonJobBuilder):
+    def born(self, dob):
+        self.person.date_of_birth = dob
+        return self
+
+
+pb = PersonBirthDateBuilder()
+
+me = pb \
+    .born('12-10-2001') \
+    .works_as_a('Genius') \
+    .called('Dimitri') \
+    .build()
+print(me)
