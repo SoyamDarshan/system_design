0bf36e9cf92c36fce4844fdaa06377905c16f96e
Soyam Darshan Pattanaik <32511312+SoyamDarshan@users.noreply.github.com>
2021-02-11 01:10:51 +0530

    added chain of responsibility pattern

---
+++ Behavioral Patterns/Chain of responsibility/CQS.py
@@ -0,0 +1,6 @@
+"""
+Command Query Separation
+Separate all invocation into two different concepts
+- Query (Asking for information without necessarily changing anything)
+- Command (Asking for an action or change)
+"""
