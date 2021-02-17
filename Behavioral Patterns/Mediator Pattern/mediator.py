44c7c8c9deea6e979c214c902a12304435ee1180
Soyam Darshan Pattanaik <32511312+SoyamDarshan@users.noreply.github.com>
2021-02-17 07:58:07 +0530

    added mediator pattern

---
+++ Behavioral Patterns/Mediator Pattern/mediator.py
@@ -0,0 +1,59 @@
+"""
+A component that facilitates communication between other components without them necessarily being aware of each other
+or having direct (references) access to each other
+"""
+
+
+# Chat room
+class Person:
+    def __init__(self, name):
+        self.name = name
+        self.chat_log = []
+        self.room = None
+
+    def receive(self, sender, message):
+        s = f'{sender}: {message}'
+        print(f'[{self.name}\'s chat session] {s}')
+        self.chat_log.append(s)
+
+    def private_message(self, who, message):
+        self.room.message(self.name, who, message)
+
+    def say(self, message):
+        self.room.broadcast(self.name, message)
+
+
+class ChatRoom:
+    def __init__(self):
+        self.people = []
+
+    def join(self, person):
+        join_msg = f'{person.name} joins the chat'
+        self.broadcast('room', join_msg)
+        person.room = self
+        self.people.append(person)
+
+    def broadcast(self, source, message):
+        for p in self.people:
+            if p.name != source:
+                p.receive(source, message)
+
+    def message(self, source, destination, message):
+        for p in self.people:
+            if p.name == destination:
+                p.receive(source, message)
+
+
+if __name__ == '__main__':
+    room = ChatRoom()
+    john = Person('John')
+    jane = Person('Jane')
+    room.join(john)
+    room.join(jane)
+    john.say('hi room!')
+    jane.say('hi john!')
+
+    simon = Person('Simon')
+    room.join(simon)
+    simon.say('Hey room')
+    simon.private_message('Jane', 'Hi jane')
